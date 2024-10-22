9f785ab52dd61866b58dcc3481b6ed736515ba79
Kunzlog001 <kunzlog001@alpinesd.org>
2024-10-22 09:23:31 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,31 @@
-#while summation code following directions from Readme
+def counting(num):
+    result = 0
+    for i in range(num):
+        result = result + i
+    finresult = result + num
+    return finresult
+
+def script():
+    user_num = int(input("Enter A Number: "))
+    choice = "w"
+    if choice.lower() == "f":
+        print(counting(user_num))
+    elif choice.lower() == "w":
+        result = 0
+        i = 0
+        while i < user_num:
+            result += i
+            i +=1
+
+        finresult = result + user_num
+        print(finresult)
+
+        
+script()
+
+restart = str(input("Restart? y/n: "))
+if restart.lower() == "y":
+    script()
+else:
+    print("byeee")
+
